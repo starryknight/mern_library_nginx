4f54c58808d6fab343b6347eee4bddf3f00decb6
starryknight <mrstanleykimani@gmail.com>
2021-04-06 01:18:33 -0400

    added .env and test script

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 require("dotenv").config();
+const express = require ("express")
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT|| 8000;
+
+app.get("/test", (req,res) => {
+    res.json({
+        Hi: "Welcome to  the MERN library API"
+    });
+});
+
+app.listen(
+    PORT,
+    console.log(
+        `server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
